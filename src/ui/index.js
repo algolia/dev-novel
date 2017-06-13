62ce9312624d7f4f911fc3db9c6f1b4a80a46bf5
iam4x <iam4x@me.com>
2017-06-13 13:25:57 +0200

    style(ui): improve container styles

--- src/ui/index.js
+++ src/ui/index.js
@@ -5,17 +5,29 @@ import styled from 'styled-components';
 
 import Sidebar from './sidebar';
 
-const Container = styled.div`
+const UIWrapper = styled.div`
   background-color: rgb(247, 247, 247);
   display: flex;
   height: 100vh;
 `;
 
-const Content = styled.div`
-  background-color: #FFF;
+const BodyWrapper = styled.div`
+  display: flex;
   flex: 1 1 0%;
   padding: 10px;
-  position: relative;
+  height: 100vh;
+`;
+
+const Body = styled.div`
+  height: 100%;
+`;
+
+const StoryContainer = styled.div`
+  background-color: #fff;
+  border: solid 1px #E4E4E4;
+  border-radius: 5px;
+  height: calc(65vh - 20px); /* -20px of inner padding */
+  overflow-x: scroll;
 `;
 
 class DevNovelUI extends Component {
@@ -23,10 +35,14 @@ class DevNovelUI extends Component {
     const { anyStorybook } = this.props;
 
     return (
-      <Container>
+      <UIWrapper>
         <Sidebar anyStorybook={anyStorybook} />
-        <Content id="story-container" />
-      </Container>
+        <BodyWrapper>
+          <Body>
+            <StoryContainer id="story-container" />
+          </Body>
+        </BodyWrapper>
+      </UIWrapper>
     );
   }
 }
