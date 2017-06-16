46856411d5b74154c2ab58384e22ed4f8a38be46
iam4x <iam4x@me.com>
2017-06-16 08:25:49 +0200

    chore(tools): extends eslint from `algolia/react` `algolia/flowtype`
    see: https://github.com/algolia/eslint-config-algolia/pull/36

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-  extends: ['algolia', 'plugin:flowtype/recommended'],
-  plugins: ['flowtype'],
+  extends: ['algolia/react', 'algolia/flowtype'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "react/prop-types": 0,
-    "react/no-string-refs": 0,
     "prettier/prettier": ['error', { trailingComma: 'es5', singleQuote: true, printWidth: 100 }]
   }
 }
-
